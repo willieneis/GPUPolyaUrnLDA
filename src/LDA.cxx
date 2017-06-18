3cc63be22b70b2d2fff45d28499b525843792e41
Kunal Sarkhel <ksarkhel@gmu.edu>
2017-06-18 13:36:21 -0400

    Add CLI skeleton

---
+++ src/LDA.cxx
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "LDA.hxx"
+
+void convertFile(std::string input_filename, std::string output_filename) {
+   std::cout << "Converting " << input_filename << " to optimized binary format" << std::endl;
+   std::cout << "Optimized file will be written to " << output_filename;
+}
+
+void LDA(std::string input_filename, std::string output_model, std::string output_state, int num_topics, int num_iterations, double alpha, double beta, int random_seed) {
+
+}
