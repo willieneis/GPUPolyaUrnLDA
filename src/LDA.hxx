f5e9b2a5993a7cec2aa36c1c3e47307429cd45ce
Kunal Sarkhel <ksarkhel@gmu.edu>
2017-06-18 13:36:21 -0400

    Add CLI skeleton

---
+++ src/LDA.hxx
@@ -0,0 +1,10 @@
+#ifndef LDA_H_
+#define LDA_H_
+
+#include <string>
+
+void convertFile(std::string input_filename, std::string output_filename);
+
+void LDA(std::string input_filename, std::string output_model, std::string output_state, int num_topics, int num_iterations, double alpha, double beta, int random_seed);
+
+#endif /* LDA_H_ */
