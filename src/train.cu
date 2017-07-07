7421ee071e3d6621386f0c9eb0cb5b760cc36bdb
aterenin <aterenin@users.noreply.github.com>
2017-07-07 13:51:58 -0400

    added partial init/cleanup code

--- src/train.cu
+++ src/train.cu
@@ -1,28 +1,43 @@
 #include "train.h"
 #include "alias.h"
 #include "dlhmatrix.h"
+#include "polyaurnsampler.h"
 #include "warpsampler.h"
 
 namespace gplda {
 
 Args ARGS;
-DLHMatrix Phi;
-DLHMatrix n;
-Poisson pois;
+DLHMatrix *Phi;
+DLHMatrix *n;
+Poisson *pois;
 
 void initialize(Args *args, Buffer *buffers, size_t n_buffers) {
-  ARGS = *args;
+  ARGS = *args; // copy from Rust
+  for(int i = 0; i < n_buffers; ++i) {
+    // allocate memory for GPU buffer
+  }
+  Phi = new DLHMatrix();
+  n = new DLHMatrix();
+  pois = new Poisson();
+}
+
+void cleanup(Buffer *buffers, size_t n_buffers) {
+  // no need to delete ARGS: it is static
+  for(int i = 0; i < n_buffers; ++i) {
+    // deallocate memory for GPU buffer
+  }
+  delete Phi;
+  delete n;
+  delete pois;
 }
 
 void sample_phi() {
+  polya_urn_sampler/*<<<1,1>>>*/();
 }
 
 void sample_z(Buffer *buffer) {
   // copy memory
-  warp_sampler(buffer->size, buffer->n_docs, buffer->gpu_z, buffer->gpu_w, buffer->gpu_d_len, buffer->gpu_d_idx);
-}
-
-void cleanup() {
+  warp_sampler/*<<<1,1>>>*/(buffer->size, buffer->n_docs, buffer->gpu_z, buffer->gpu_w, buffer->gpu_d_len, buffer->gpu_d_idx);
 }
 
 } // namespace gplda ends here
@@ -38,6 +53,6 @@ int main(void) {
   initialize(&args, &buffer, 1);
   sample_phi();
   sample_z(&buffer);
-  cleanup();
+  cleanup(&buffer, 1);
   return 0;
 }
