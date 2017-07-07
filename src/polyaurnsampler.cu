68358b8304fdf9ce644c182810a4a154bce2199f
aterenin <aterenin@users.noreply.github.com>
2017-07-07 13:51:58 -0400

    added partial init/cleanup code

---
+++ src/polyaurnsampler.cu
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <iostream>
+#include "train.h"
+
+
+/*__global__*/ void polya_urn_sampler() {
+  std::cout << "launching poisson polya urn sampler" << std::endl;
+}
---
+++ src/polyaurnsampler.h
@@ -0,0 +1,1 @@
+void polya_urn_sampler();
