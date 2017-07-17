b87bc69c63388dc13db4b2eed4f9229a2e0814bb
aterenin <aterenin@users.noreply.github.com>
2017-07-17 16:28:00 -0400

    added pseudocode for Alias table construction

--- src/cu/poisson.cu
+++ src/cu/poisson.cu
@@ -15,7 +15,7 @@ __device__ __forceinline__ unsigned int lane_offset(unsigned int lane_bits, int 
 }
 
 __global__ void build_poisson(float** prob, float** alias, float beta, int table_size) {
-  assert(blockDim.x == 32); // for simplicity, Poisson Alias tables are built on the warp level, so exit if misconfigured
+//  assert(blockDim.x == 32); // for simplicity, Poisson Alias tables are built on the warp level, so exit if misconfigured
   int lambda = blockIdx.x; // each block builds one table
   float L = lambda + beta;
   // populate PMF
@@ -28,35 +28,56 @@ __global__ void build_poisson(float** prob, float** alias, float beta, int table
   }
   __syncthreads();
   // build array of large probabilities
-  /*extern*/ __shared__ float large[200];
+  /*extern*/ __shared__ int large[200];
   __shared__ int num_large[1];
   if(threadIdx.x == 0) {
     num_large[0] = 0;
   }
+  __syncthreads();
   float cutoff = 1.0/((float) table_size);
   // loop over PMF
   for(int offset = 0; offset < table_size / blockDim.x + 1; ++offset) {
     int i = threadIdx.x + offset * blockDim.x;
     if(i < table_size) {
-      // determine which warps have large probabilities
       float thread_prob = prob[lambda][i];
-      unsigned int warp_large = __ballot(thread_prob >= cutoff);
+      // determine which threads have large probabilities
+      unsigned int warp_large_bits = __ballot(thread_prob >= cutoff);
       // determine how many large probabilities are in the warp's view
-      int warp_num_large = __popc(warp_large);
+      int warp_num_large = __popc(warp_large_bits);
       // increment the array's size, only once per warp, then broadcast to all lanes in the warp
-      int large_start;
+      int warp_large_start;
       if(threadIdx.x % warpSize == 0) {
-        large_start = atomicAdd(num_large, warp_num_large);
+        warp_large_start = atomicAdd(num_large, warp_num_large);
       }
-      large_start = __shfl(large_start, 0);
+      warp_large_start = __shfl(warp_large_start, 0);
       // if current warp has elements, add elements to the array
       if(thread_prob >= cutoff) {
-        large[large_start + lane_offset(warp_large, threadIdx.x)] = thread_prob;
+        large[warp_large_start + lane_offset(warp_large_bits, threadIdx.x)] = i;
       }
     }
   }
   __syncthreads();
-  // we've built large array, let's grab elements and place them
+  // grab a set of indices from large array for the warp to work on
+
+  // loop over each warp's range
+
+    // try to place probabilities into current window
+
+    // place any small probabilities into slots and small index array, grab new probability
+
+    // if large stack empty, perform a warp rebalance
+
+    // if large stack empty and cannot rebalance, write current index to window stack
+
+  __syncthreads();
+  // if still holding indices, grab an index from window stack and iterate over that
+
+
+  __syncthreads();
+  // if window stack empty, grab a set of elements from small stack and iterate over that
+
+  __syncthreads();
+  // at this point, all remaining slots must have probability 1
 }
 
 
@@ -85,7 +106,7 @@ Poisson::Poisson(int ml, int mv) {
   delete[] prob_host;
   delete[] alias_host;
   // launch kernel to build the alias tables
-  build_poisson<<<max_lambda,32,max_value*sizeof(int)>>>(prob, alias, ARGS->beta, max_value);
+  build_poisson<<<max_lambda,64/*32*/,max_value*sizeof(int)>>>(prob, alias, ARGS->beta, max_value);
   cudaDeviceSynchronize();
 }
 
