38f18c8a858d080e7f2ce5a91e22d71c8a703d19
aterenin <aterenin@users.noreply.github.com>
2017-07-19 12:51:30 -0400

    queue pair in Alias build (much better)

--- src/cu/poisson.cu
+++ src/cu/poisson.cu
@@ -6,20 +6,50 @@
 namespace gplda {
 
 
-__device__ __forceinline__ unsigned int warp_lane_id_bits(int thread_idx) {
-  return ((unsigned int) 1) << (thread_idx % warpSize);
+__device__ __forceinline__ unsigned int warp_lane_id_bits(int lane_idx) {
+  return ((unsigned int) 1) << lane_idx;
 }
 
-__device__ __forceinline__ unsigned int warp_lane_offset(unsigned int lane_bits, int thread_idx) {
-  return __popc((~(((unsigned int) 4294967295) << (thread_idx % warpSize))) & lane_bits);
+__device__ __forceinline__ unsigned int warp_lane_offset(unsigned int lane_bits) {
+  return __popc((~(((unsigned int) 4294967295) << (threadIdx.x % 32))) & lane_bits);
 }
 
-__device__ __forceinline__ int queue_pop(int* queue, int* start, int* end) {
+__device__ __forceinline__ int queue_wraparound(int idx) {
+  return idx;
+}
+
+__device__ __forceinline__ void warp_queue_pair_push(int value, int conditional, int* q1, int* q2, int* q1_end, int* q2_end) {
+  // determine which threads write to which queue
+  unsigned int warp_q1_bits = __ballot(conditional);
+  unsigned int warp_q2_bits = __ballot(~conditional); // note: some threads may be inactive
+  // determine how many writes are in the warp's view for each queue
+  int warp_num_q1 = __popc(warp_q1_bits);
+  int warp_num_q2 = __popc(warp_q2_bits);
+  // increment the queue's size, only once per warp, then broadcast to all lanes in the warp
+  int warp_q1_start;
+  int warp_q2_start;
+  if(threadIdx.x % 32 == 0) {
+    warp_q1_start = atomicAdd(q1_end, warp_num_q1);
+    warp_q2_start = atomicAdd(q2_end, warp_num_q2);
+  }
+  warp_q1_start = __shfl(warp_q1_start, 0);
+  warp_q2_start = __shfl(warp_q2_start, 0);
+  // if current thread has elements, determine where to write them
+  int* write_queue;
+  int write_idx;
+  if(conditional) {
+    write_queue = q1;
+    write_idx = warp_q1_start + warp_lane_offset(warp_q1_bits);
+  } else {
+    write_queue = q2;
+    write_idx = warp_q2_start + warp_lane_offset(warp_q2_bits);
+  }
+  // write elements to both queues
+  write_queue[queue_wraparound(write_idx)] = value;
+}
+
+__device__ __forceinline__ int warp_queue_pair_pop(int& size, int* start, int* end1, int* end2) {
   return 1;
-}
-
-__device__ __forceinline__ void queue_push(int value, int* queue, int* start, int* end) {
-  return;
 }
 
 __global__ void build_poisson(float** prob, float** alias, float beta, int table_size) {
@@ -42,17 +72,15 @@ __global__ void build_poisson(float** prob, float** alias, float beta, int table
   __syncthreads();
   // initialize queues
   __shared__ int num_active_warps[1];
+  __shared__ int queue_pair_start[1];
   /*extern*/ __shared__ int large[200];
-  __shared__ int large_start[1];
   __shared__ int large_end[1];
   /*extern*/ __shared__ int small[200];
-  __shared__ int small_start[1];
   __shared__ int small_end[1];
   if(threadIdx.x == 0) {
     num_active_warps[0] = num_warps;
-    large_start[0] = 0;
+    queue_pair_start[0] = 0;
     large_end[0] = 0;
-    small_start[0] = 0;
     small_end[0] = 0;
   }
   __syncthreads();
@@ -61,77 +89,34 @@ __global__ void build_poisson(float** prob, float** alias, float beta, int table
     int i = threadIdx.x + offset * blockDim.x;
     if(i < table_size) {
       float thread_prob = prob[lambda][i];
-      // determine which threads have large probabilities
-      unsigned int warp_large_bits = __ballot(thread_prob >= cutoff);
-      unsigned int warp_small_bits = __ballot(thread_prob < cutoff); // don't negate: some threads may be inactive
-      // determine how many large probabilities are in the warp's view
-      int warp_num_large = __popc(warp_large_bits);
-      int warp_num_small = __popc(warp_small_bits);
-      // increment the array's size, only once per warp, then broadcast to all lanes in the warp
-      int warp_large_start;
-      int warp_small_start;
-      if(lane_idx == 0) {
-        warp_large_start = atomicAdd(large_end, warp_num_large);
-        warp_small_start = atomicAdd(small_end, warp_num_small);
-      }
-      warp_large_start = __shfl(warp_large_start, 0);
-      warp_small_start = __shfl(warp_small_start, 0);
-      // if current warp has elements, add elements to the array
-      if(thread_prob >= cutoff) {
-        large[warp_large_start + warp_lane_offset(warp_large_bits, threadIdx.x)] = i;
-      } else {
-        small[warp_small_start + warp_lane_offset(warp_small_bits, threadIdx.x)] = i;
-      }
+      warp_queue_pair_push(i, thread_prob >= cutoff, large, large_end, small, small_end);
     }
   }
-  __syncthreads();
-  // grab a set of indices from large queue for the thread to work on
-  int thread_large_idx = -1; /* flag value: -1 means empty */
-  float thread_large_prob;
-  while(true) {
-    // if needed, grab indices from large queue for the warp to work on
-    if(thread_large_idx < 0) {
-      // try to grab an index
-      thread_large_idx = queue_pop(large, large_start, large_end);
-      // if got an index, get its value
-      if(thread_large_idx >= 0) {
-        thread_large_prob = prob[lambda][thread_large_idx];
+
+  // grab a set of indices from both queues for the warp to work on
+  for(int warp_num_elements = warpSize; warp_num_elements > 0; /*no increment*/) {
+    // try to grab an index, determine how many were grabbed
+    warp_num_elements = warpSize;
+    int warp_queue_idx = warp_queue_pair_pop(warp_num_elements, queue_pair_start, large_end, small_end);
+    // if got an index, fill it
+    if(lane_idx < warp_num_elements) {
+      int thread_large_idx = large[queue_wraparound(warp_queue_idx + lane_idx)];
+      float thread_large_prob = prob[lambda][thread_large_idx];
+      int thread_small_idx = small[queue_wraparound(warp_queue_idx + lane_idx)];
+      float thread_small_prob = prob[lambda][thread_small_idx];
+      // determine new probability and fill the index
+      thread_large_prob = (thread_large_prob + thread_small_prob) - 1.0;
+      alias[lambda][thread_small_idx] = thread_large_idx;
+      // if large prob became small, write it to its corresponding slot
+      if(thread_large_prob < cutoff) {
+        prob[lambda][thread_large_idx] = thread_large_prob;
       }
-    }
-    // if holding a large index, try to fill a small index
-    if(thread_large_idx >= 0) {
-      // try to grab an index from small queue
-      int thread_small_idx = queue_pop(small, small_start, small_end);
-      // if got an index, fill it
-      if(thread_small_idx >= 0) {
-        float thread_small_prob = prob[lambda][thread_small_idx];
-        thread_large_prob = (thread_large_prob + thread_small_prob) - 1.0;
-        alias[lambda][thread_small_idx] = thread_large_idx;
-        // check if large probability became small
-        if(thread_large_prob < cutoff) {
-          // make prob small
-          prob[lambda][thread_large_idx] = thread_large_prob;
-          // add to small queue
-          queue_push(thread_large_idx, small, small_start, small_end);
-          thread_large_idx = -1;
-        }
-      } else {
-        // if small queue is empty, push value back onto large queue, and exit
-        queue_push(thread_large_idx, large, large_start, large_end);
-        thread_large_idx = -1;
-        break;
-      }
-    } else {
-      // large queue is empty, exit
-      break;
+      // finally, push remaining values back onto queues
+      warp_queue_pair_push(thread_large_idx, thread_large_prob >= cutoff, large, large_end, small, small_end);
     }
   }
   // at this point, both queues should now be near empty, so finish them using one warp
-  if(warp_idx != 0) {
-    atomicSub(num_active_warps, 1);
-    return;
-  } else {
-    do {/*wait*/} while(num_active_warps[0] > 1);
+  if(atomicSub(num_active_warps, 1) == 1) {
     printf("Finishing remaining values");
   }
 }
