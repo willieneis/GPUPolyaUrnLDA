b55ca7bb5a448e0e6f46d8f333f381bc76d9ac07
aterenin <aterenin@users.noreply.github.com>
2017-08-16 14:16:12 -0400

    partial commit of hashmap implementation

--- src/cu/hashmap.cuh
+++ src/cu/hashmap.cuh
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "stdint.h"
+#include "tuning.cuh"
+#include <curand_kernel.h> // need to add -lcurand to nvcc flags
 
 #define GPLDA_EMPTY_HASH 0xffffffff00000000
 
@@ -13,30 +15,60 @@ struct HashMap {
   uint64_t* stash;
   uint64_t* temp_data;
   uint64_t* temp_stash;
+  uint64_t a[GPLDA_HASH_NUM_FUNCTIONS];
+  uint64_t b[GPLDA_HASH_NUM_FUNCTIONS];
 };
 
-__device__ __forceinline__ void hash_map_init(HashMap* map, void* temp, uint32_t size, int32_t dim) {
+__device__ __forceinline__ int32_t hash_map_hash_function(int32_t k, int32_t a, int32_t b, int32_t s) {
+  return (a * k + b) % 334214459 % s;
+}
+
+__device__ inline void hash_map_init(HashMap* map, void* temp, uint32_t size, int32_t dim, curandStatePhilox4_32_10_t* rng) {
   // determine constants
-  int32_t lane_idx = threadIdx.x % dim;
+  int32_t thread_idx = threadIdx.x % dim;
 
   // set map parameters and calculate random hash functions
   map->size = size;
   map->num_elements = 0;
   map->data = (uint64_t*) temp;
-
-  // set map to zero
-  for(int32_t offset = 0; offset < size / dim + 1; ++offset) {
-    // misaligned address?
-    map->data[offset * dim + lane_idx] = GPLDA_EMPTY_HASH;
+  map->stash = map->data + size*sizeof(uint64_t);
+  map->temp_data = map->stash + GPLDA_HASH_STASH_SIZE*sizeof(uint64_t);
+  map->temp_stash = map->temp_data + size*sizeof(uint64_t);
+  for(int32_t i = 0; i < GPLDA_HASH_NUM_FUNCTIONS; ++i) {
+    map->a[i] = __uint2float_rz(size * curand_uniform(rng));
+    map->b[i] = __uint2float_rz(size * curand_uniform(rng));
   }
-}
 
-__device__ __forceinline__ void hash_map_increment(uint32_t key, HashMap* map) {
+  // set map to empty
+  for(int32_t offset = 0; offset < size / dim + 1; ++offset) {
+    int32_t i = offset * dim + thread_idx;
+    if(i < size) {
+      map->data[i] = GPLDA_EMPTY_HASH;
+    }
+  }
 
+  // set stash to empty
+  for(int32_t offset = 0; offset < GPLDA_HASH_STASH_SIZE / dim + 1; ++offset) {
+    int32_t i = offset * dim + thread_idx;
+    if(i < GPLDA_HASH_STASH_SIZE) {
+      map->stash[i] = GPLDA_EMPTY_HASH;
+    }
+  }
 }
 
 __device__ __forceinline__ uint32_t hash_map_get(uint32_t key, HashMap* map) {
   return 0;
 }
 
+__device__ __forceinline__ void hash_map_set(uint32_t key, HashMap* map) {
+
+}
+
+__device__ __forceinline__ void hash_map_increment(uint32_t key, HashMap* map) {
+  uint32_t value = hash_map_get(key, map);
+}
+
+__device__ __forceinline__ void hash_map_decrement(uint32_t key, HashMap* map) {
+  uint32_t value = hash_map_get(key, map);
+}
 }
--- src/cu/warpsample.cu
+++ src/cu/warpsample.cu
@@ -1,6 +1,7 @@
 #include "hashmap.cuh"
 #include "warpsample.cuh"
 #include <thrust/system/cuda/detail/cub/block/block_scan.cuh>
+#include <thrust/system/cuda/detail/cub/warp/warp_scan.cuh>
 
 namespace gplda {
 
@@ -34,34 +35,40 @@ __global__ void compute_d_idx(uint32_t* d_len, uint32_t* d_idx, uint32_t n_docs)
   }
 }
 
-__device__ __forceinline__ uint32_t draw_alias(float u, float* prob, uint32_t* alias, uint32_t table_size) {
-  // determine the slot and update random number
-  float ts = (float) table_size;
-  uint32_t slot = (uint32_t) (u * ts);
-  u = fmodf(u, __frcp_rz(ts)) * ts;
+__device__ __forceinline__ uint32_t draw_alias(float u, float* prob, uint32_t* alias, uint32_t table_size, int32_t lane_idx) {
+  uint32_t ret = 0;
+  if(lane_idx == 0) {
+    // determine the slot and update random number
+    float ts = (float) table_size;
+    uint32_t slot = (uint32_t) (u * ts);
+    u = fmodf(u, __frcp_rz(ts)) * ts;
 
-  // load table elements from global memory
-  float thread_prob = prob[slot];
-  uint32_t thread_alias = alias[slot];
+    // load table elements from global memory
+    float thread_prob = prob[slot];
+    uint32_t thread_alias = alias[slot];
 
-  // return the resulting draw
-  if(u < thread_prob) {
-    return slot;
-  } else {
-    return thread_alias;
+    // return the resulting draw
+    if(u < thread_prob) {
+      ret = slot;
+    } else {
+      ret = thread_alias;
+    }
   }
+  return ret;
 }
 
 __device__ __forceinline__ uint32_t draw_wary_search(float u) {
   return 0;
 }
 
-__device__ __forceinline__ void count_topics(uint32_t* z, uint32_t document_size, HashMap* m, void* temp) {
-  hash_map_init(m, temp, document_size, warpSize);
+__device__ __forceinline__ void count_topics(uint32_t* z, uint32_t document_size, HashMap* m, void* temp, curandStatePhilox4_32_10_t* rng) {
+  hash_map_init(m, temp, document_size, warpSize, rng);
   // loop over z, add to m
 }
 
-__device__ __forceinline__ float compute_product_cumsum(uint32_t* mPhi) {
+__device__ __forceinline__ float compute_product_cumsum(uint32_t* mPhi, HashMap* m, float* Phi_dense, int32_t warp_idx, cub::WarpScan<int32_t>::TempStorage* temp) {
+  int32_t thread_mPhi = 0;
+  cub::WarpScan<int32_t>(temp[warp_idx]).ExclusiveSum(thread_mPhi, thread_mPhi);
   return 0.0f;
 }
 
@@ -75,35 +82,48 @@ __global__ void warp_sample_topics(uint32_t size, uint32_t n_docs,
   HashMap m;
   uint32_t** mPhi = (uint32_t**) &m.temp_data;
   int32_t lane_idx = threadIdx.x % warpSize;
+  int32_t warp_idx = threadIdx.x / warpSize;
+  __shared__ typename cub::WarpScan<int32_t>::TempStorage warp_scan_temp[1];
 
   // loop over documents
   for(int32_t i = 0; i < n_docs; ++i) {
     // count topics in document
     uint32_t warp_d_len = d_len[i];
     uint32_t warp_d_idx = d_idx[i];
-    count_topics(z + warp_d_idx * sizeof(uint32_t), warp_d_len, &m, temp);
+    count_topics(z + warp_d_idx * sizeof(uint32_t), warp_d_len, &m, temp, &warp_rng);
 
     // loop over words
     for(int32_t j = 0; j < warp_d_len; ++j) {
-      // compute m*phi and sigma_b
+      // load z,w from global memory
       uint32_t warp_z = z[warp_d_idx + j];
-      uint32_t warp_w = w[warp_d_idx + j];
+      uint32_t warp_w = 0;//w[warp_d_idx + j];
+
+      // remove current z from sufficient statistic
+      if(lane_idx == 0) {
+        hash_map_decrement(warp_z, &m);
+      }
+
+      // compute m*phi and sigma_b
       float warp_sigma_a = 0.0f;
-      float sigma_b = compute_product_cumsum(*mPhi);
+      float sigma_b = compute_product_cumsum(*mPhi, &m, Phi_dense, warp_idx, warp_scan_temp);
 
       // update z
       float u1 = curand_uniform(&warp_rng);
       float u2 = curand_uniform(&warp_rng);
       if(u1 * (warp_sigma_a + sigma_b) > warp_sigma_a) {
         // sample from m*Phi
-        z[warp_d_idx + j] = draw_wary_search(u2);
+        warp_z = draw_wary_search(u2);
       } else {
         // sample from alias table
-        z[warp_d_idx + j] = draw_alias(u2, prob[0], alias[0], /*table_size =*/ 1);
+        warp_z = draw_alias(u2, prob[warp_w], alias[warp_w], /*table_size =*/ 1, lane_idx);
       }
 
+      // add new z to sufficient statistic
+      if(lane_idx == 0) {
+        hash_map_increment(warp_z, &m);
+        z[warp_d_idx + j] = warp_z;
+      }
     }
-
   }
 }
 
