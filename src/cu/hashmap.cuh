0ff2816279d3efdc6059128b5482a835470460d3
aterenin <aterenin@users.noreply.github.com>
2017-08-17 16:23:32 -0400

    fixed a few bugs

--- src/cu/hashmap.cuh
+++ src/cu/hashmap.cuh
@@ -49,6 +49,9 @@ struct HashMap {
   }
 
 
+
+
+
   __device__ inline void init(void* temp, u32 size, curandStatePhilox4_32_10_t* rng) {
     // calculate initialization variables common for all threads
     i32 dim = (sync_type == block) ? blockDim.x : warpSize;
@@ -92,11 +95,14 @@ struct HashMap {
       }
     }
 
-    // synchronize if block map
+    // synchronize to ensure initialization is complete
     if(sync_type == block) {
      __syncthreads();
     }
   }
+
+
+
 
 
   __device__ inline void rebuild(u64 kv) {
@@ -105,7 +111,7 @@ struct HashMap {
       return;
     }
 
-    // synchronize if block map
+    // synchronize to ensure rebuilding has been read everywhere
     if(sync_type == block) {
      __syncthreads();
     }
@@ -123,7 +129,6 @@ struct HashMap {
       this->stash = this->temp_stash;
       this->temp_data = d;
       this->temp_stash = s;
-      i32 thread_idx = threadIdx.x % this->access_dim;
       #pragma unroll
       for(i32 i = 0; i < GPLDA_HASH_NUM_FUNCTIONS; ++i) {
        this->a[i] = __float2uint_rz(size * curand_uniform(this->rng));
@@ -133,7 +138,7 @@ struct HashMap {
       this->b_stash = __float2uint_rz(size * curand_uniform(this->rng));
     }
 
-    // synchronize if block map
+    // synchronize to ensure pointers have been swapped
     if(sync_type == block) {
      __syncthreads();
     }
@@ -146,7 +151,7 @@ struct HashMap {
     u64* temp_stash = this->temp_stash;
 
     // set map to empty
-    for(i32 offset = 0; offset < size / this->access_dim + 1; ++offset) {
+    for(i32 offset = 0; offset < size / dim + 1; ++offset) {
       i32 i = offset * dim + thread_idx;
       if(i < size) {
         data[i] = GPLDA_HASH_EMPTY;
@@ -155,14 +160,14 @@ struct HashMap {
 
     // set stash to empty
     #pragma unroll
-    for(i32 offset = 0; offset < GPLDA_HASH_STASH_SIZE / this->access_dim + 1; ++offset) {
+    for(i32 offset = 0; offset < GPLDA_HASH_STASH_SIZE / dim + 1; ++offset) {
       i32 i = offset * dim + thread_idx;
       if(i < GPLDA_HASH_STASH_SIZE) {
         stash[i] = GPLDA_HASH_EMPTY;
       }
     }
 
-    // synchronize if block map
+    // synchronize to ensure table is empty
     if(sync_type == block) {
       __syncthreads();
     }
@@ -172,8 +177,8 @@ struct HashMap {
 
     // insert elements that were in stash
     #pragma unroll
-    for(i32 offset = 0; offset < GPLDA_HASH_STASH_SIZE / this->access_dim + 1; ++offset) {
-      i32 i = offset * this->access_dim + thread_idx;
+    for(i32 offset = 0; offset < GPLDA_HASH_STASH_SIZE / dim + 1; ++offset) {
+      i32 i = offset * dim + thread_idx;
       if(i < GPLDA_HASH_STASH_SIZE) {
         kv = temp_stash[i];
         set(kv);
@@ -181,8 +186,8 @@ struct HashMap {
     }
 
     // iterate over map and place remaining keys
-    for(i32 offset = 0; offset < size / this->access_dim + 1; ++offset) {
-      i32 i = offset * this->access_dim + thread_idx;
+    for(i32 offset = 0; offset < size / dim + 1; ++offset) {
+      i32 i = offset * dim + thread_idx;
       if(i < size) {
         kv = temp_data[i];
         set(kv);
@@ -190,29 +195,43 @@ struct HashMap {
     }
 
     // rebuild is done
-    this->rebuilding = false;
+    if(thread_idx == 0) {
+      this->rebuilding = false;
+    }
 
-    // synchronize if block map
+    // synchronize to ensure rebuilding is set to false everywhere
     if(sync_type == block) {
       __syncthreads();
     }
   }
 
 
+
+
+
   __device__ __forceinline__ u32 get(u32 key) {
+    // get thread-specific variables
+    u32* a = this->a;
+    u32* b = this->b;
+    u64* data = this->data;
+    u64* stash = this->stash;
+    u32 a_stash = this->a_stash;
+    u32 b_stash = this->b_stash;
+    u32 size = this->size;
+
     // check table
     #pragma unroll
     for(i32 i = 0; i < GPLDA_HASH_NUM_FUNCTIONS; ++i) {
-      i32 slot = hash_fn(key, this->a[i], this->b[i], this->size);
-      u64 kv = this->data[slot];
+      i32 slot = hash_fn(key, a[i], b[i], size);
+      u64 kv = data[slot];
       if(left_32_bits(kv) == key) {
         return right_32_bits(kv);
       }
     }
 
     // check stash
-    i32 slot = hash_fn(key, this->a_stash, this->b_stash, GPLDA_HASH_STASH_SIZE);
-    u64 kv = this->data[slot];
+    i32 slot = hash_fn(key, a_stash, b_stash, GPLDA_HASH_STASH_SIZE);
+    u64 kv = stash[slot];
     if(left_32_bits(kv) == key) {
       return right_32_bits(kv);
     }
@@ -220,6 +239,9 @@ struct HashMap {
     // no value: return zero
     return 0;
   }
+
+
+
 
 
   __device__ __forceinline__ void set(u64 kv) {
@@ -232,22 +254,27 @@ struct HashMap {
     u32 b_stash = this->b_stash;
     u32 size = this->size;
 
-    // repeatedly try to insert values
-    u32 current_a = a[0];
-    u32 current_b = b[0];
-    for(i32 i = 0; i < GPLDA_HASH_MAX_ITERATIONS; ++i) {
-      i32 slot = hash_fn(kv, current_a, current_b, size);
-      kv = atomicExch(&data[slot],kv);
+    // if we have a value, repeatedly try to insert values
+    if(right_32_bits(kv) != 0) {
+      u32 current_a = a[0];
+      u32 current_b = b[0];
+      for(i32 i = 0; i < GPLDA_HASH_MAX_ITERATIONS; ++i) {
+        i32 slot = hash_fn(kv, current_a, current_b, size);
+        kv = atomicExch(&data[slot],kv);
 
-      // if slot was empty, exit
-      if(kv == GPLDA_HASH_EMPTY) {
-        break; // don't return: might need to rebuild on that warp
+        // if slot was empty, exit
+        if(kv == GPLDA_HASH_EMPTY) {
+          break; // don't return: might need to rebuild on that warp
+        }
+
+        // determine which hash function was used, try again
+        i32 j = hash_idx(kv, slot, a, b, size);
+        current_a = a[j+1 % GPLDA_HASH_NUM_FUNCTIONS];
+        current_b = b[j+1 % GPLDA_HASH_NUM_FUNCTIONS];
       }
-
-      // determine which hash function was used, try again
-      i32 j = hash_idx(kv, slot, a, b, size);
-      current_a = a[j+1];
-      current_b = b[j+1];
+    } else {
+      // we're setting a key to zero, so do nothing
+      kv = GPLDA_HASH_EMPTY;
     }
 
     // if key is still present, try stash
@@ -256,20 +283,33 @@ struct HashMap {
       kv = atomicExch(&stash[slot], kv);
     }
 
-    // synchronize if block map
+    // check if stash collided, and if so, rebuild table
     if(sync_type == block) {
+      // need to synchronize and broadcast to ensure entire block enters rebuild
       __syncthreads();
-    }
-
-    // if key is still present, stash collided, so rebuild table
-    if(kv != GPLDA_HASH_EMPTY) {
-      rebuild(kv);
+      if(kv != GPLDA_HASH_EMPTY) {
+        this->rebuilding = 2; // flag value to indicate needs rebuild
+      }
+      __syncthreads();
+      if(this->rebuilding == 2) {
+        rebuild(kv);
+      }
+    } else {
+      if(kv != GPLDA_HASH_EMPTY) {
+        rebuild(kv);
+      }
     }
   }
+
+
+
 
   __device__ __forceinline__ void set(u32 key, u32 value) {
     set((u64) key << 32 | value);
   }
+
+
+
 
   __device__ __forceinline__ void accumulate(u32 key, i32 diff) {
     u32 value = get(key);
