fe7d9d99656d8503d660bd569a60e63cf0c575f7
aterenin <aterenin@users.noreply.github.com>
2017-09-14 15:21:08 -0400

    fixed a few bugs in try_accumulate2

--- src/cu/hashmap_robinhood.cu
+++ src/cu/hashmap_robinhood.cu
@@ -276,12 +276,11 @@ struct HashMap {
     u32 half_lane_mask = 0x0000ffff << (((threadIdx.x % warpSize) / 16) * 4); // 4 if lane >= 16, 0 otherwise
 
     // build entry to be inserted and shuffle to entire half warp
-    HashMapEntry halfwarp_entry = entry(false,GPLDA_HASH_NULL_POINTER,key,diff);
-    halfwarp_entry.int_repr = __shfl(halfwarp_entry.int_repr, 0, warpSize/2);
+    HashMapEntry half_warp_entry = entry(false,GPLDA_HASH_NULL_POINTER,key,diff);
+    half_warp_entry.int_repr = __shfl(half_warp_entry.int_repr, 0, warpSize/2);
 
     // insert key into linked queue
     HashMapEntry thread_table_entry;
-    u32 half_warp_write;
     i32 initial_slot = hash_slot(key,a,b);
     i32 stride = hash_slot(key,c,d);
     for(i32 i = 0; i < GPLDA_HASH_MAX_NUM_LINES; ++i) {
@@ -301,28 +300,42 @@ struct HashMap {
         u32 thread_no_key = key_distance(thread_table_entry.key, slot) > i;
 
         // determine which thread should write
-        half_warp_write = __ballot(thread_found_key | thread_found_empty | thread_no_key) & half_lane_mask;
+        u32 half_warp_found_key = __ballot(thread_found_key) & half_lane_mask;
+        u32 half_warp_found_empty = __ballot(thread_found_empty) & half_lane_mask;
+        u32 half_warp_no_key = __ballot(thread_no_key) & half_lane_mask;
+
+        u32 half_warp_write;
+        if(half_warp_found_key != 0) {
+          half_warp_write = half_warp_found_key;
+        } else if(half_warp_found_empty != 0) {
+          half_warp_write = half_warp_found_empty;
+        } else if(half_warp_no_key != 0) {
+          half_warp_write = half_warp_no_key;
+        } else {
+          half_warp_write = 0;
+        }
         u32 lane_write_idx = __ffs(half_warp_write) - 1; // __ffs uses 1-based indexing
 
-        if(lane_idx == lane_write_idx) {
+        if(half_warp_write != 0 && lane_idx == lane_write_idx) {
           // prepare new entry for table
           u32 buffer_idx = GPLDA_HASH_NULL_POINTER;
 
           // determine what kind of new entry we have
           if(thread_found_key == true) {
             // key found: accumulate value
-            halfwarp_entry.value += thread_table_entry.value;
+            half_warp_entry.value += thread_table_entry.value;
           } else if(thread_no_key == true) {
             // Robin Hood guarantee indicates no key present: insert into eviction queue
             buffer_idx = ring_buffer_pop();
-            ring_buffer[buffer_idx] = halfwarp_entry;
+            ring_buffer[buffer_idx] = half_warp_entry;
 
             // prepare new entry
-            halfwarp_entry.pointer = buffer_idx;
+            half_warp_entry = thread_table_entry;
+            half_warp_entry.pointer = buffer_idx;
           }
 
           // swap new and old entry
-          u64 old_entry_int_repr = atomicCAS(&thread_table_entry.int_repr, thread_table_entry.int_repr, halfwarp_entry.int_repr);
+          u64 old_entry_int_repr = atomicCAS(&thread_table_entry.int_repr, thread_table_entry.int_repr, half_warp_entry.int_repr);
 
           // make sure retrieved entry matches what was expected, so we know that CAS succeeded
           if(old_entry_int_repr != thread_table_entry.int_repr) {
@@ -333,22 +346,28 @@ struct HashMap {
             if(buffer_idx != GPLDA_HASH_NULL_POINTER) {
               ring_buffer[buffer_idx] = entry(false, GPLDA_HASH_NULL_POINTER, GPLDA_HASH_EMPTY, 0);
               ring_buffer_push(buffer_idx);
+              half_warp_entry.pointer = GPLDA_HASH_NULL_POINTER;
+            } else {
+              half_warp_entry = entry(false, GPLDA_HASH_NULL_POINTER, GPLDA_HASH_EMPTY, 0);
             }
           }
         }
+
+        // ensure entire halfwarp knows whether write succeeded
+        half_warp_entry.int_repr = __shfl(half_warp_entry.int_repr, 0, warpSize/2);
 
         // ensure retry, if necessary, is performed on entire half warp
         retry = __ballot(retry) & half_lane_mask;
       } while(retry != 0);
 
       // if half warp successfully performed a write, exit the loop
-      if(half_warp_write != 0) {
+      if(half_warp_entry.int_repr == entry(false, GPLDA_HASH_NULL_POINTER, GPLDA_HASH_EMPTY, 0).int_repr) {
         break;
       }
     }
 
     // check to make sure insertion succeeded: if it failed, return
-    if(half_warp_write == 0) {
+    if(half_warp_entry.int_repr != entry(false, GPLDA_HASH_NULL_POINTER, GPLDA_HASH_EMPTY, 0).int_repr) {
       return thread_table_entry;
     }
 
